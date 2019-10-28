9ce234bca72dcb28157941bf9fa097314bf3a26b
Daniel Sanandaj <daniel.sanandaj@gmail.com>
2019-10-27 21:29:19 -0700

    First commit for total budget

--- app/src/main/java/com/example/knowyourlimit/LoginActivity.java
+++ app/src/main/java/com/example/knowyourlimit/LoginActivity.java
@@ -1,23 +1,35 @@
 package com.example.knowyourlimit;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.widget.EditText;
+import android.text.Html;
 import android.widget.TextView;
+import java.text.DecimalFormat;
 
-import org.w3c.dom.Text;
+public class LoginActivity extends AppCompatActivity{
 
-public class LoginActivity extends AppCompatActivity {
+    public double totalBudget, transportation, housing, food, miscellaneous, initialBudget = 18767.566;
+    private TextView budgetView;
+    DecimalFormat df = new DecimalFormat("#.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        budgetView = (TextView) findViewById(R.id.textView2);
 
-        TextView something = (TextView)findViewById(R.id.textView);
-        something.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
-        something.setText("You are very gay");
+        totalBudget = initialBudget - (transportation + housing + food + miscellaneous);
+
+        if(totalBudget < 0){
+            String totalString = "<font color='black'>Total Budget: </font>" + "<font color='red'>"
+                    + "\u0024" + df.format(totalBudget) + "</font>";
+            budgetView.setText(Html.fromHtml(totalString), TextView.BufferType.SPANNABLE);
+        }
+        else {
+            String totalString = "<font color='black'>Total Budget: </font>" + "<font color='green'>"
+                    + "\u0024" + df.format(totalBudget) + "</font>";
+            budgetView.setText(Html.fromHtml(totalString), TextView.BufferType.SPANNABLE);
+        }
+
     }
 }
