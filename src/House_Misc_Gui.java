6c97a13600eed32cdb1cb395b6b53079f438e00e
BeanieBoy <alexander.cloutiernosik.684@my.csun.edu>
2019-11-21 19:46:16 -0800

    want tp pull from master

--- src/House_Misc_Gui.java
+++ src/House_Misc_Gui.java
@@ -1,3 +1,27 @@
-public class House_Misc_Gui {
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
+public class House_Misc_Gui {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button myButton = new Button(this);
+        RelativeLayout myLayout = new RelativeLayout(this);
+        myLayout.addView(myButton);
+        setContentView(myLayout);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
